ebe1489d25bd85743ccc64017d51bfc384e75973
Dohoon Gwak <dogwak@cx2r7s10.42seoul.kr>
2024-08-06 20:56:49 +0900

    \#4 define camera to view converter.

--- src/raytracer.h
+++ src/raytracer.h
@@ -6,7 +6,7 @@
 /*   By: dogwak <dogwak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 11:21:17 by dogwak            #+#    #+#             */
-/*   Updated: 2024/08/05 14:08:31 by dogwak           ###   ########.fr       */
+/*   Updated: 2024/08/06 20:37:20 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,28 @@
 # define RAYTRACER_H
 
 # include "./ft_graphics/ft_graphics.h"
+# include "ray.h"
 
-// virtual camera
+typedef struct s_render_resource
+{
+	t_FTMLX		ftmlx;
+	t_FTCAMERA	cam;
+	t_FTMFLOAT4	*points;
+	// TODO : global ambient light
+	// TODO : vector of lights
+	// TODO : vector of render target objects
+}				t_render_resource;
 
-// rt routine
+// resource initialize
+// int			set_render_resource(t_render_resource *self, char **argv);
+// void			destruct_render_resource(t_render_resource *self);
+void			set_points(t_render_resource *const prsrc);
+void			convert_points(t_render_resource *const prsrc);
+
+// render
+// TODO : rendering routine
+
+// etc
+float			clamp(float f);
 
 #endif
