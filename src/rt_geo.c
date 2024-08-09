467b8c6fa078cc6f04d2fa98a296eb7812ddd79d
Dohoon Gwak <dogwak@cx2r8s1.42seoul.kr>
2024-08-09 20:06:55 +0900

    \#16 feat : implement geometries, rt_obj's init routine.

---
+++ src/rt_geo.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rt_geo.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dogwak <dogwak@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/09 19:52:18 by dogwak            #+#    #+#             */
+/*   Updated: 2024/08/09 20:00:06 by dogwak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt_object.h"
+#include <stdlib.h>
+
+t_plane	*new_plane(t_obj_desc *d)
+{
+	t_plane *const	self = malloc(sizeof(t_plane));
+
+	if (self == NULL)
+		return (NULL);
+	self->m = d->m;
+	self->pcenter = d->p1;
+	self->vnormal = d->p2;
+	return (self);
+}
+
+t_sphere	*new_sphere(t_obj_desc *d)
+{
+	t_sphere *const	self = malloc(sizeof(t_sphere));
+
+	if (self == NULL)
+		return (NULL);
+	self->m = d->m;
+	self->pcenter = d->p1;
+	self->radius = d->val;
+	return (self);
+}
+
+t_cylinder	*new_cylinder(t_obj_desc *d)
+{
+	t_cylinder *const	self = malloc(sizeof(t_cylinder));
+
+	if (self == NULL)
+		return (NULL);
+	self->m = d->m;
+	self->pcenter1 = d->p1;
+	self->pcenter2 = d->p2;
+	self->radius = d->val;
+	return (self);
+}
+
+t_cone	*new_cone(t_obj_desc *d)
+{
+	t_cone *const	self = malloc(sizeof(t_cone));
+
+	if (self == NULL)
+		return (NULL);
+	self->m = d->m;
+	self->pcenter = d->p1;
+	self->pvertex = d->p2;
+	self->radius = d->val;
+	return (self);
+}
