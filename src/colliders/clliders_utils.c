57a9a3751c233aff1e109b32e3aa974058b61ece
HyWChoi <109526914+HyWChoi@users.noreply.github.com>
2024-08-17 18:52:48 +0900

    fix(colliders): delete unused func

--- src/colliders/clliders_utils.c
+++ src/colliders/clliders_utils.c
@@ -5,10 +5,3 @@ t_FTMFLOAT4 ray_at(t_ray *ray, float t)
 {
 	return (ftmf4_qadd(ray->pstart, vmult(&ray->ndir, t)));
 }
-
-int	rt_fabs(float val)
-{
-	if (val < 0)
-		return (-val);
-	return (val);
-}
