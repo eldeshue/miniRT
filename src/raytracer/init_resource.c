1e18e64ad97868ea6d949f2012be1755f73630d0
Dohoon Gwak <dogwak@cx2r7s9.42seoul.kr>
2024-08-22 20:50:22 +0900

    \#28 feat : add light object to the test.

--- src/raytracer/init_resource.c
+++ src/raytracer/init_resource.c
@@ -6,7 +6,7 @@
 /*   By: dogwak <dogwak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 14:39:55 by dogwak            #+#    #+#             */
-/*   Updated: 2024/08/22 17:47:06 by dogwak           ###   ########.fr       */
+/*   Updated: 2024/08/22 20:42:48 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,13 @@ int	init_render_resource(t_render_resource *self, char **argv)
 	// test code
 	// camera init
 	self->fov = 90;
-	self->cam.vpos = ftmf4_set_vector(-100.0f, 0.0f, 100.0f, 1.0f);
-	self->cam.vtarget = ftmf4_set_vector(1.0f, 0.0f, -0.3f, 0.0f);
+	self->cam.vpos = ftmf4_set_vector(0.0f, -300.0f, 300.0f, 1.0f);
+	self->cam.vtarget = ftmf4_set_vector(0.0f, 1.0f, -1.0f, 0.0f);
 	ftmf4_vnormalize(&self->cam.vtarget);
 	self->cam.vup = ftmf4_set_vector(0.0f, 0.0f, 1.0f, 0.0f);
 
 	// global ambient color setting
-	self->amb_color = ftmf4_set_vector(255.0f, 255.0f, 255.0f, 0.0f);
+	self->amb_color = ftmf4_set_vector(55.0f, 55.0f, 0.0f, 0.0f);
 	self->amb_intens = 0.3;
 
 	// object init
@@ -42,10 +42,10 @@ int	init_render_resource(t_render_resource *self, char **argv)
 	tmp.p1 = ftmf4_set_vector(200.0f, 0.0f, 100.0f, 1.0f);
 	tmp.val = 40.0f;
 	tmp.type = sphere;
-	self->render_objects->push_back(self->render_objects, &tmp);
+	//self->render_objects->push_back(self->render_objects, &tmp);
 
 	// plane
-	tmp.m.obj_color = ftmf4_set_vector(55.0f, 55.0f, 200.0f, 0.0f);	// blue
+	tmp.m.obj_color = ftmf4_set_vector(55.0f, 55.0f, 55.0f, 0.0f);	// blue
 	tmp.p1 = ftmf4_set_vector(0.0f, 0.0f, 0.0f, 1.0f);
 	tmp.p2 = ftmf4_set_vector(0.0f, 0.0f, 1.0f, 0.0f);	// normal vector of plane
 	tmp.val = 0.0f;
@@ -55,8 +55,13 @@ int	init_render_resource(t_render_resource *self, char **argv)
 	// light init
 	t_light	l;
 
-	l.ppos = ftmf4_set_vector(0.0f, 0.0f, 500.0f, 1.0f);
-	l.color = ftmf4_set_vector(255.0f, 255.0f, 255.0f, 0.0f);
+	l.ppos = ftmf4_set_vector(0.0f, 1500.0f, 1500.0f, 1.0f);
+	l.color = ftmf4_set_vector(255.0f, 30.0f, 30.0f, 0.0f);
+	l.intensity = 0.6f;
+	self->lights->push_back(self->lights, &l);
+
+	l.ppos = ftmf4_set_vector(0.0f, 0.0f, 1500.0f, 1.0f);
+	l.color = ftmf4_set_vector(30.0f, 255.0f, 30.0f, 0.0f);
 	l.intensity = 0.6f;
 	self->lights->push_back(self->lights, &l);
 
