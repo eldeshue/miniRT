f9a1a51b46d2d0436c8f9696ef2b9b4abdeb1b4a
Dohoon Gwak <dogwak@cx2r8s4.42seoul.kr>
2024-08-26 15:32:11 +0900

    \#28 feat : total test.

--- src/raytracer/init_resource.c
+++ src/raytracer/init_resource.c
@@ -6,7 +6,7 @@
 /*   By: dogwak <dogwak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 14:39:55 by dogwak            #+#    #+#             */
-/*   Updated: 2024/08/23 20:08:12 by dogwak           ###   ########.fr       */
+/*   Updated: 2024/08/23 20:35:50 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,33 +24,33 @@ int	init_render_resource(t_render_resource *self, char **argv)
 
 	// test code
 	// camera init
-	self->fov = 90;
-	self->cam.vpos = ftmf4_set_vector(0.0f, -300.0f, 300.0f, 1.0f);
-	self->cam.vtarget = ftmf4_set_vector(0.0f, 1.0f, -1.0f, 0.0f);
+	self->fov = 120;
+	self->cam.vpos = ftmf4_set_vector(0.0f, -100.0f, 150.0f, 1.0f);
+	self->cam.vtarget = ftmf4_set_vector(0.0f, 1.0f, -1.7f, 0.0f);
 	ftmf4_vnormalize(&self->cam.vtarget);
 	self->cam.vup = ftmf4_set_vector(0.0f, 0.0f, 1.0f, 0.0f);
 
 	// global ambient color setting
-	self->amb_color = ftmf4_set_vector(55.0f, 55.0f, 0.0f, 0.0f);
-	self->amb_intens = 0.3;
+	self->amb_color = ftmf4_set_vector(255.0f, 255.0f, 255.0f, 0.0f);
+	self->amb_intens = 0.5;
 
 	// object init
 	t_obj_desc	tmp;
 
 	// sphere
-	//self->render_objects->push_back(self->render_objects, &tmp);
-	// tmp.p1 = ftmf4_set_vector(100.0f, 100.0f, 50.0f, 1.0f);        // 중심 좌표 (100, 100, 50)
-	// tmp.val = 30.0f;                                                // 반지름 30
-	// tmp.type = sphere;
-	// self->render_objects->push_back(self->render_objects, &tmp);
+	tmp.m.obj_color = ftmf4_set_vector(255.0f, 55.0f, 55.0f, 0.0f);	// green
+	tmp.p1 = ftmf4_set_vector(0.0f, 0.0f, 40.0f, 1.0f);        // 중심 좌표 (100, 100, 50)
+	tmp.val = 30.0f;                                                // 반지름 30
+	tmp.type = sphere;
+	self->render_objects->push_back(self->render_objects, &tmp);
 
 	// cylinder
-	tmp.m.obj_color = ftmf4_set_vector(0.0f, 255.0f, 0.0f, 0.0f);	// green
-	tmp.p1 = ftmf4_set_vector(100.0f, 0.0f, 80.0f, 1.0f);
-	tmp.p2 = ftmf4_set_vector(100.0f, 0.0f, 10.0f, 0.0f);	// normal vector of cylinder
-	tmp.val = 40.0f;
+	tmp.m.obj_color = ftmf4_set_vector(55.0f, 255.0f, 55.0f, 0.0f);	// green
+	tmp.p1 = ftmf4_set_vector(60.0f, 0.0f, 60.0f, 1.0f);
+	tmp.p2 = ftmf4_set_vector(60.0f, 0.0f, 10.0f, 0.0f);	// normal vector of cylinder
+	tmp.val = 20.0f;
 	tmp.type = cylinder;
-	// self->render_objects->push_back(self->render_objects, &tmp);
+	self->render_objects->push_back(self->render_objects, &tmp);
 
 	// plane
 	tmp.m.obj_color = ftmf4_set_vector(55.0f, 55.0f, 55.0f, 0.0f);	// blue
@@ -58,12 +58,12 @@ int	init_render_resource(t_render_resource *self, char **argv)
 	tmp.p2 = ftmf4_set_vector(0.0f, 0.0f, 1.0f, 0.0f);	// normal vector of plane
 	tmp.val = 0.0f;
 	tmp.type = plane;
-	// self->render_objects->push_back(self->render_objects, &tmp);
+	self->render_objects->push_back(self->render_objects, &tmp);
 
 	// cone
-	tmp.m.obj_color = ftmf4_set_vector(255.0f, 255.0f, 0.0f, 0.0f);	// red
-	tmp.p2 = ftmf4_set_vector(100.0f, 0.0f, 80.0f, 1.0f);  // vertex
-	tmp.p1 = ftmf4_set_vector(100.0f, 0.0f, 10.0f, 1.0f);  // center
+	tmp.m.obj_color = ftmf4_set_vector(55.0f, 55.0f, 255.0f, 0.0f);	// red
+	tmp.p2 = ftmf4_set_vector(-80.0f, 0.0f, 80.0f, 1.0f);  // vertex
+	tmp.p1 = ftmf4_set_vector(-80.0f, 0.0f, 0.0f, 1.0f);  // center
 	tmp.val = 40.0f;
 	tmp.type = cone;
 	self->render_objects->push_back(self->render_objects, &tmp);
@@ -71,13 +71,13 @@ int	init_render_resource(t_render_resource *self, char **argv)
 	// light init
 	t_light	l;
 
-	l.ppos = ftmf4_set_vector(0.0f, 1500.0f, 1500.0f, 1.0f);
+	l.ppos = ftmf4_set_vector(-300.0f, 0.0f, 500.0f, 1.0f);
 	l.color = ftmf4_set_vector(255.0f, 30.0f, 30.0f, 0.0f);
 	l.intensity = 0.6f;
 	self->lights->push_back(self->lights, &l);
 
-	l.ppos = ftmf4_set_vector(0.0f, 0.0f, 1500.0f, 1.0f);
-	l.color = ftmf4_set_vector(30.0f, 255.0f, 30.0f, 0.0f);
+	l.ppos = ftmf4_set_vector(300.0f, 0.0f, 500.0f, 1.0f);
+	l.color = ftmf4_set_vector(30.0f, 30.0f, 255.0f, 0.0f);
 	l.intensity = 0.6f;
 	self->lights->push_back(self->lights, &l);
 
