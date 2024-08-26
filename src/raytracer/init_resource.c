5b96f8ee46318b2cfb07c4fe7e011aa41e87ec53
Dohoon Gwak <dogwak@cx2r8s4.42seoul.kr>
2024-08-26 16:36:16 +0900

    \#28 feat : modify test codes.

--- src/raytracer/init_resource.c
+++ src/raytracer/init_resource.c
@@ -6,7 +6,7 @@
 /*   By: dogwak <dogwak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 14:39:55 by dogwak            #+#    #+#             */
-/*   Updated: 2024/08/23 20:35:50 by dogwak           ###   ########.fr       */
+/*   Updated: 2024/08/26 16:12:49 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ int	init_render_resource(t_render_resource *self, char **argv)
 	// test code
 	// camera init
 	self->fov = 120;
-	self->cam.vpos = ftmf4_set_vector(0.0f, -100.0f, 150.0f, 1.0f);
-	self->cam.vtarget = ftmf4_set_vector(0.0f, 1.0f, -1.7f, 0.0f);
+	self->cam.vpos = ftmf4_set_vector(0.0f, -50.0f, 80.0f, 1.0f);
+	self->cam.vtarget = ftmf4_set_vector(0.0f, 1.0f, -0.5f, 0.0f);
 	ftmf4_vnormalize(&self->cam.vtarget);
 	self->cam.vup = ftmf4_set_vector(0.0f, 0.0f, 1.0f, 0.0f);
 
@@ -45,12 +45,12 @@ int	init_render_resource(t_render_resource *self, char **argv)
 	self->render_objects->push_back(self->render_objects, &tmp);
 
 	// cylinder
-	tmp.m.obj_color = ftmf4_set_vector(55.0f, 255.0f, 55.0f, 0.0f);	// green
-	tmp.p1 = ftmf4_set_vector(60.0f, 0.0f, 60.0f, 1.0f);
-	tmp.p2 = ftmf4_set_vector(60.0f, 0.0f, 10.0f, 0.0f);	// normal vector of cylinder
+	tmp.m.obj_color = ftmf4_set_vector(55.0f, 155.0f, 55.0f, 0.0f);	// green
+	tmp.p1 = ftmf4_set_vector(0.0f, 0.0f, 60.0f, 1.0f);
+	tmp.p2 = ftmf4_set_vector(0.0f, 0.0f, 10.0f, 0.0f);	// normal vector of cylinder
 	tmp.val = 20.0f;
 	tmp.type = cylinder;
-	self->render_objects->push_back(self->render_objects, &tmp);
+	//self->render_objects->push_back(self->render_objects, &tmp);
 
 	// plane
 	tmp.m.obj_color = ftmf4_set_vector(55.0f, 55.0f, 55.0f, 0.0f);	// blue
@@ -62,24 +62,24 @@ int	init_render_resource(t_render_resource *self, char **argv)
 
 	// cone
 	tmp.m.obj_color = ftmf4_set_vector(55.0f, 55.0f, 255.0f, 0.0f);	// red
-	tmp.p2 = ftmf4_set_vector(-80.0f, 0.0f, 80.0f, 1.0f);  // vertex
-	tmp.p1 = ftmf4_set_vector(-80.0f, 0.0f, 0.0f, 1.0f);  // center
+	tmp.p2 = ftmf4_set_vector(0.0f, 0.0f, 70.0f, 1.0f);  // vertex
+	tmp.p1 = ftmf4_set_vector(0.0f, 0.0f, 30.0f, 1.0f);  // center
 	tmp.val = 40.0f;
 	tmp.type = cone;
-	self->render_objects->push_back(self->render_objects, &tmp);
+	//self->render_objects->push_back(self->render_objects, &tmp);
 
 	// light init
 	t_light	l;
 
-	l.ppos = ftmf4_set_vector(-300.0f, 0.0f, 500.0f, 1.0f);
-	l.color = ftmf4_set_vector(255.0f, 30.0f, 30.0f, 0.0f);
-	l.intensity = 0.6f;
+	l.ppos = ftmf4_set_vector(0.0f, -80.0f, 180.0f, 1.0f);
+	l.color = ftmf4_set_vector(155.0f, 155.0f, 155.0f, 0.0f);
+	l.intensity = 0.2f;
 	self->lights->push_back(self->lights, &l);
 
 	l.ppos = ftmf4_set_vector(300.0f, 0.0f, 500.0f, 1.0f);
-	l.color = ftmf4_set_vector(30.0f, 30.0f, 255.0f, 0.0f);
+	l.color = ftmf4_set_vector(255.0f, 255.0f, 255.0f, 0.0f);
 	l.intensity = 0.6f;
-	self->lights->push_back(self->lights, &l);
+	//self->lights->push_back(self->lights, &l);
 
 	set_view_plane(self);	// call set_view_plane after camera initialization.
 	return (1);
